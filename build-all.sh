33a9ab2621ab4faff0feeeb893c02748ea1a94e6
Jurrian Fahner <jurrian.fahner@gmail.com>
2021-11-06 21:45:58 +0100

    update docker-compose

--- build-all.sh
+++ build-all.sh
@@ -7,12 +7,12 @@ then
     exit 2
 fi
 
-: ${1? "Usage: $0 DOCKER_REGISTRY_NAME_URL e.g. ensignprojects/"}
+: ${1? "Usage: $0 DOCKER_REGISTRY_NAME_URL e.g. docker.io/ensignprojects/"}
 
 echo "Build all containers"
 
 echo "-- ubuntu-compose"
-./ubuntu-compose/build.sh $1 "1.29.0"
+./ubuntu-compose/build.sh $1 "v2.1.0"
 
 echo "-- ubuntu-curl"
 ./ubuntu-curl/build.sh $1
