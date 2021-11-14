dd90e07c25d989ebc6dbd0701a5f302fb77829b9
Jurrian Fahner <jurrian.fahner@gmail.com>
2021-11-14 22:02:52 +0100

    change name docker container to dnsutils

--- ubuntu-dnsutils/build.sh
+++ ubuntu-dnsutils/build.sh
@@ -4,9 +4,9 @@ date_tag=$(date +%Y%m%d)
 
 docker build --pull \
              -q \
-             -t ${1}ubuntu-dig:latest \
-             -t ${1}ubuntu-dig:$date_tag \
+             -t ${1}ubuntu-dnsutils:latest \
+             -t ${1}ubuntu-dnsutils:$date_tag \
              $(dirname $(realpath $0))
 
-docker push -q ${1}ubuntu-dig:latest
-docker push -q ${1}ubuntu-dig:$date_tag
+docker push -q ${1}ubuntu-dnsutils:latest
+docker push -q ${1}ubuntu-dnsutils:$date_tag
